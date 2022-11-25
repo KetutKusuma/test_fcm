7f9b0ac43ee8aeda0160d4c522ca97763d799f76
Ketut Kusuma <71806737+KetutKusuma@users.noreply.github.com>
2022-11-25 14:58:58 +0800

    update bos

--- lib/main.dart
+++ lib/main.dart
@@ -2,33 +2,70 @@ import 'package:firebase_core/firebase_core.dart';
 import 'package:firebase_messaging/firebase_messaging.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_fcm_laravel_test/firebase_options.dart';
-import 'package:firebase_analytics/firebase_analytics.dart';
+import 'package:flutter_local_notifications/flutter_local_notifications.dart';
+
+/**
+ * ini coba test
+ */
+// Future<void> _firebaseMessagingBackgroundHandler(RemoteMessage message) async {
+//   // If you're going to use other Firebase services in the background, such as Firestore,
+//   // make sure you call `initializeApp` before using other Firebase services.
+//   await Firebase.initializeApp();
+
+//   print("Handling a background message: ${message.messageId}");
+// }
+
+const AndroidNotificationChannel channel = AndroidNotificationChannel(
+    'high_importance_channel', // id
+    'High Importance Notifications', // title
+    description:
+        'This channel is used for important notifications.', // description
+    importance: Importance.high,
+    playSound: true);
+
+final FlutterLocalNotificationsPlugin flutterLocalNotificationsPlugin =
+    FlutterLocalNotificationsPlugin();
 
 Future<void> _firebaseMessagingBackgroundHandler(RemoteMessage message) async {
-  // If you're going to use other Firebase services in the background, such as Firestore,
-  // make sure you call `initializeApp` before using other Firebase services.
   await Firebase.initializeApp();
-
-  print("Handling a background message: ${message.messageId}");
+  print('A bg message just showed up :  ${message.messageId}');
 }
 
-Future main() async {
-  await WidgetsFlutterBinding
-      .ensureInitialized(); //harus ini dulu diinisialisasi
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized(); //harus ini dulu diinisialisasi
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
   ); //baru ini
-  FirebaseMessaging messaging = FirebaseMessaging.instance;
-  FirebaseMessaging.onBackgroundMessage(_firebaseMessagingBackgroundHandler);
-  FirebaseMessaging.onMessage.listen((RemoteMessage message) {
-    print("mama ${message.data}");
-    print('Got a message whilst in the foreground!');
-    print('Message data: ${message.data}');
 
-    if (message.notification != null) {
-      print('Message also contained a notification: ${message.notification}');
-    }
-  });
+  /**
+   * Handler for background message
+   */
+  FirebaseMessaging.onBackgroundMessage(_firebaseMessagingBackgroundHandler);
+
+  await flutterLocalNotificationsPlugin
+      .resolvePlatformSpecificImplementation<
+          AndroidFlutterLocalNotificationsPlugin>()!
+      .createNotificationChannel(channel);
+
+  await FirebaseMessaging.instance.setForegroundNotificationPresentationOptions(
+    alert: true,
+    badge: true,
+    sound: true,
+  );
+
+  /**
+   * ini tut test
+   */
+  // FirebaseMessaging messaging = FirebaseMessaging.instance;
+  // FirebaseMessaging.onMessage.listen((RemoteMessage message) {
+  //   print("mama ${message.data}");
+  //   print('Got a message whilst in the foreground!');
+  //   print('Message data: ${message.data}');
+
+  //   if (message.notification != null) {
+  //     print('Message also contained a notification: ${message.notification}');
+  //   }
+  // });
 
   FirebaseMessaging.instance.getToken().then(
         (value) => print(value),
@@ -72,12 +109,82 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    FirebaseMessaging.onMessage.listen((RemoteMessage message) {
+      RemoteNotification? notification = message.notification;
+      AndroidNotification? android = message.notification?.android;
+      if (notification != null && android != null) {
+        flutterLocalNotificationsPlugin.show(
+          notification.hashCode,
+          notification.title,
+          notification.body,
+          NotificationDetails(
+            android: AndroidNotificationDetails(
+              channel.id, channel.name,
+              channelDescription: notification.body,
+              // channel.description,
+              color: Colors.blue,
+              playSound: true,
+              icon: "@mipmap/ic_launcher",
+            ),
+          ),
+        );
+      }
+      print("mama no message");
+    });
+    // FirebaseMessaging.onMessage.
 
-  void _incrementCounter() {
+    FirebaseMessaging.onMessageOpenedApp.listen(
+      (RemoteMessage message) {
+        print('A new onMessageOpenedApp event was published!!!');
+        RemoteNotification? notification = message.notification;
+        AndroidNotification? android = message.notification?.android;
+        if (notification != null && android != null) {
+          showDialog(
+            context: context,
+            builder: (_) {
+              return AlertDialog(
+                title: Text(notification.title.toString()),
+                content: SingleChildScrollView(
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text(
+                        notification.body.toString(),
+                      ),
+                    ],
+                  ),
+                ),
+              );
+            },
+          );
+        }
+      },
+    );
+  }
+
+  int _counter = 0;
+  void showNotification() {
     setState(() {
       _counter++;
     });
+    flutterLocalNotificationsPlugin.show(
+      0,
+      "Testing $_counter",
+      "How you doing? \n\n\n mama" + "mama",
+      NotificationDetails(
+        android: AndroidNotificationDetails(channel.id, channel.name,
+            // channel.description,
+            importance: Importance.high,
+            // subText: "mama ohhhh mama",
+            color: Colors.blue,
+            playSound: true,
+            icon: "@mipmap/ic_launcher"),
+      ),
+    );
   }
 
   @override
@@ -105,9 +212,9 @@ class _MyHomePageState extends State<MyHomePage> {
         ),
       ),
       floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
+        onPressed: showNotification,
+        tooltip: "Show Increment",
+        child: Icon(Icons.add),
       ),
     );
   }
